0dfa8a1c2b4ac3a543f978d5f2993434098b6b80
mrozek <01178582@pw.edu.pl>
2024-06-13 22:02:26 +0200

    REFACTOR: Changed LoginPage button class for custom class

--- src/LoginPage/LoginPage.js
+++ src/LoginPage/LoginPage.js
@@ -66,7 +66,7 @@ const LoginPage = () => {
                         <div>
                             <button
                                 type="submit"
-                                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-lightRed transition duration-300 hover:bg-hoverRed focus:outline-none focus:ring-2 focus:ring-offset-2"
+                                className="login-register-button-base"
                             >
                                 Sign in
                             </button>
