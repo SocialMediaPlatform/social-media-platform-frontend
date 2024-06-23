ccfe80b05277e87ebdc6a50dd460a62968857d08
Władysław Młynik <01168900@pw.edu.pl>
2024-06-23 14:50:26 +0200

    IMPLEMENT: Added user's posts to profile page

--- src/ProfilePage/ProfilePage.js
+++ src/ProfilePage/ProfilePage.js
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from "../AuthContext";
 import Header from '../HomePage/components/Header';
+import Post from '../HomePage/components/Post';
 
 const ProfilePage = () => {
     const { userToken } = useContext(AuthContext);
@@ -9,6 +10,11 @@ const ProfilePage = () => {
         username: 'mock',
         email: 'mock@example.com'
     })
+
+    const [posts, setPosts] = useState([
+        { id: 1, username: 'mock', likes: 5, comments: 2, isLiked: false, content: 'Test post 1\ndkamskldmaskldmklasmdklas\nmakldmaskldmklasmdklasm\nmaskdmaskldmaskldmaklsm' },
+        { id: 2, username: 'mock', likes: 3, comments: 1, isLiked: true, content: 'Test post 2' },
+    ]);
 
     // This should load the user data from the API, but the API doesn't seem to have that feature yet
     // useEffect(() => {
@@ -35,13 +41,38 @@ const ProfilePage = () => {
     //     }
     // }, [userToken]);
 
+    // This should work, but uses hardcoded user id for now
+    useEffect(() => {
+        if (userToken) {
+            const fetchPosts = async () => {
+                try {
+                    const response = await fetch('/api/v1/post/1', {
+                        method: 'GET',
+                        headers: {
+                            'Authorization': `Bearer ${userToken}`
+                        },
+                        credentials: 'include'
+                    });
+                    if (!response.ok) {
+                        throw new Error(`${response.status}`);
+                    }
+                    const result = await response.json();
+                    setPosts(result);
+                } catch (error) {
+                    console.error('Error fetching posts:', error);
+                }
+            };
+            fetchPosts();
+        }
+    }, [userToken]);
+
     return (
         <div className='flex flex-col min-h-screen'>
             <Header />
             <div className='bg-backgroundGrey flex flex-1 justify-center items-stretch'>
                 <div className='box-border border border-t-backgroundGrey border-borderGrey flex-col flex-grow max-w-2xl bg-backgroundGrey'>
                     <div className='p-4 border-b border-borderGrey'>
-                        <div className='flex flex-row items-center border-b border-borderGrey break-words transition duration-300 hover:bg-hoverBackgroundGrey p-4'>
+                        <div className='flex flex-row items-center p-4'>
                             <div className='rounded-full h-20 w-20 flex items-center justify-center bg-avatarGrey text-white text-5xl'>
                                 {user.username[0].toUpperCase()}
                             </div>
@@ -49,15 +80,22 @@ const ProfilePage = () => {
                                 <p className='text-white text-3xl'>{user.username}</p>
                             </div>
                         </div>
-                        <div className='flex flex-row items-center pt-4'>
-                            <div className='px-4 border-r border-borderGrey break-words transition duration-300 hover:bg-hoverBackgroundGrey'>
-                                <p className='text-white text-xl'>e-mail</p>
+                        {
+                            user.email ?
+                            <div className='flex flex-row items-center pt-4 border-t border-borderGrey'>
+                                <div className='px-4 border-r border-borderGrey'>
+                                    <p className='text-white text-xl'>e-mail</p>
+                                </div>
+                                <div className='ml-4 flex-grow'>
+                                    <p className='text-white text-xl'>{user.email}</p>
+                                </div>
                             </div>
-                            <div className='ml-4 flex-grow'>
-                                <p className='text-white text-xl'>{user.email}</p>
-                            </div>
-                        </div>
+                            : <></>
+                        }
                     </div>
+                {posts.map(post => (
+                    <Post post={post} />
+                ))}
                 </div>
             </div>
         </div>
