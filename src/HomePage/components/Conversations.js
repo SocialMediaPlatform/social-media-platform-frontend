f304512eb817ffff51afcd7a89fac9515913d428
mrozek <01178582@pw.edu.pl>
2024-06-23 20:18:16 +0200

    IMPLEMENT: Message Modal is finished, adding endpoints for sending message and adding user to conversation

--- src/HomePage/components/Conversations.js
+++ src/HomePage/components/Conversations.js
@@ -5,7 +5,6 @@ import { AuthContext } from '../../AuthContext';
 
 const Conversations = () => {
     const userToken = useContext(AuthContext); 
-    const [selectedUserOrGroup, setSelectedUserOrGroup] = useState(null);
     const [conversationContent, setConversationContent] = useState(null);
     const [isModalOpen, setIsModalOpen] = useState(false);
 
@@ -34,23 +33,74 @@ const Conversations = () => {
             };
             fetchConversationContent();
             if (!conversationContent){
-                
+                setConversationContent({usernames: [user.username]});
             }
+            setIsModalOpen(true);
         }
-        setSelectedUserOrGroup(user);
-         
-        setIsModalOpen(true);
     };
 
     const handleGroupSelect = (group) => {
-        setSelectedUserOrGroup(group);
-        setIsModalOpen(true);
+        if (userToken) {
+            //const fetchConversationContent = async () => {
+            //    try {
+            //        const response = await fetch(`/api/v1/conversations/${group.id}`, {
+            //            method: 'GET',
+            //            headers: {
+            //                'Authorization': `Bearer ${userToken}`
+            //            },
+            //            credentials: 'include'
+            //        });
+            //        if (!response.ok) {
+            //            throw new Error(`${response.status}`);
+            //        }
+            //        const result = await response.json();
+            //        setConversationContent(result);
+            //    } catch (error) {
+            //        console.error('Error fetching conversation content:', error);
+            //    }
+            //};
+            //fetchConversationContent();
+
+            setConversationContent({
+                    conversationId: 1, 
+                    recipients: [
+                            {
+                            userId: 1,
+                            username: 'maksde25'
+                            },
+                            {
+                            userId: 2,
+                            username: 'lajsu69'
+                            }
+                            ], 
+                    messages:[
+                            {
+                            messageId: 1, 
+                            messageContent: "kocham lajsa",
+                            messageDate: "23-06-2024",
+                            senderId: 1
+                            },
+                            {
+                            messageId: 2, 
+                            messageContent: "beeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+                            messageDate: "23-06-2024",
+                            senderId: 2
+                            }, 
+                            {
+                            messageId: 3, 
+                            messageContent: "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+                            messageDate: "23-06-2024",
+                            senderId: 0
+                            }] 
+            });
+            setIsModalOpen(true);
+        }
     };
 
     return (
-        <div className="fixed right-0 top-18 w-1/6 h-full flex overflow-auto max-h-[calc(100%-4.5rem)]">
+        <div className='fixed right-0 top-18 w-1/6 h-full flex overflow-auto max-h-[calc(100%-4.5rem)]'>
             <ConvSidebar users={users} groups={groups} userSelect={handleUserSelect} groupSelect={handleGroupSelect} />
-            {isModalOpen && <ConvModal content={`Konwersacja z ${selectedUserOrGroup.name}`} closeModal={() => setIsModalOpen(false)} />}
+            {isModalOpen && <ConvModal content={conversationContent} closeModal={() => setIsModalOpen(false)} />}
         </div>
     );
 };
