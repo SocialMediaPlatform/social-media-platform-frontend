12eab31664b324e077feb671c27987c0f2c97281
Władysław Młynik <01168900@pw.edu.pl>
2024-06-24 23:55:06 +0200

    IMPLEMENT: Added reactions to comments

--- src/PostPage/components/Comment.js
+++ src/PostPage/components/Comment.js
@@ -1,15 +1,67 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from "../../AuthContext";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faThumbsUp, faComments, faThumbsDown } from '@fortawesome/free-solid-svg-icons';
 import AddComment from './AddComment';
 
 const Comment = ({ comment, fetchPost }) => {
-    const [isLiked, setIsLiked] = useState(comment.isLiked);
+    const [isLiked, setIsLiked] = useState(false);
+    const [isDisliked, setIsDisliked] = useState(false);
     const [repliesOpen, setRepliesOpen] = useState(false);
-    const [likesCount, setLikesCount] = useState(comment.reactionsCount);
+    const [likesCount, setLikesCount] = useState(0);
+    const [dislikesCount, setDislikesCount] = useState(0);
+    const [reactions, setReactions] = useState([])
+
     const { userToken, userId, apiUrl } = useContext(AuthContext);
     console.log(comment)
+
+    const addReaction = async (reaction) => {
+        try {
+            const response = await fetch(apiUrl + '/api/v1/reaction/addCommentReaction', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${userToken}`
+                },
+                credentials: 'include',
+                body: JSON.stringify({
+                    commentId: comment.commentId,
+                    reactionTypeId: reaction,
+                    userId: userId
+                })
+            });
+            if (!response.ok) {
+                throw new Error(`${response.status}`);
+            }
+            fetchPost();
+        } catch (error) {
+            console.error('Error adding reaction:', error);
+        }
+    }
+
+    const removeReaction = async (reaction) => {
+        try {
+            const response = await fetch(apiUrl + '/api/v1/reaction/removeCommentReaction', {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${userToken}`
+                },
+                credentials: 'include',
+                body: JSON.stringify({
+                    commentId: comment.commentId,
+                    reactionTypeId: reaction,
+                    userId: userId
+                })
+            });
+            if (!response.ok) {
+                throw new Error(`${response.status}`);
+            }
+            fetchPost();
+        } catch (error) {
+            console.error('Error adding reaction:', error);
+        }
+    }
 
     const toggleLike = async () => {
         try {
@@ -18,10 +70,48 @@ const Comment = ({ comment, fetchPost }) => {
 
             setLikesCount(newIsLiked ? likesCount + 1 : likesCount - 1);
 
+            if(newIsLiked) {
+                if(isDisliked) {
+                    await removeReaction(4);
+                    setIsDisliked(false);
+                    setDislikesCount(dislikesCount - 1);
+                }
+                await addReaction(1);
+            }
+            else {
+                await removeReaction(1);
+            }
+
 
 
         } catch (error) {
             console.error('Error toggling like:', error);
+            setIsLiked(isLiked);
+            setLikesCount(likesCount);
+        }
+    };
+
+    const toggleDislike = async () => {
+        try {
+            const newIsDisliked = !isDisliked;
+            setIsDisliked(newIsDisliked);
+
+            setDislikesCount(newIsDisliked ? dislikesCount + 1 : dislikesCount - 1);
+
+            if(newIsDisliked) {
+                if(isLiked) {
+                    await removeReaction(1);
+                    setIsLiked(false);
+                    setLikesCount(likesCount - 1)
+                }
+                await addReaction(4);
+            }
+            else {
+                await removeReaction(4);
+            }
+
+        } catch (error) {
+            console.error('Error toggling dislike:', error);
             setIsLiked(isLiked);
             setLikesCount(likesCount);
         }
@@ -52,9 +142,37 @@ const Comment = ({ comment, fetchPost }) => {
             }
             fetchPost();
         } catch (error) {
-            console.error('Error sending post:', error);
+            console.error('Error sending reply:', error);
         }
     };
+
+    const fetchReactions = async () => {
+        try {
+            const response = await fetch(apiUrl + '/api/v1/comment/reactions/' + comment.commentId, {
+                method: 'GET',
+                headers: {
+                    'Authorization': `Bearer ${userToken}`
+                },
+                credentials: 'include'
+            });
+            if (!response.ok) {
+                throw new Error(`${response.status}`);
+            }
+            const result = await response.json();
+            setIsLiked(result.some(obj => (obj.user.userId == userId) && (obj.reactionTypeId === 1)));
+            setIsDisliked(result.some(obj => (obj.user.userId == userId) && (obj.reactionTypeId === 4)));
+            setLikesCount(result.filter(obj => obj.reactionTypeId === 1).length);
+            setDislikesCount(result.filter(obj => obj.reactionTypeId === 4).length);
+        } catch (error) {
+            console.error('Error fetching reactions:', error);
+        }
+    };
+
+    useEffect (() => {
+        if(userToken) {
+            fetchReactions();
+        }
+    }, [])
 
     return (
         <div className='border-b border-borderGrey'>
@@ -76,9 +194,9 @@ const Comment = ({ comment, fetchPost }) => {
                                 </button>
                             </div>
                             <div className='w-1/6'>
-                                <button onClick={toggleLike} className='flex items-center'>
-                                    <FontAwesomeIcon icon={faThumbsDown} className={`mr-2 transition duration-200 ${isLiked ? 'hover:text-hoverRed text-lightRed' : 'hover:text-hoverTextGrey text-textGrey'}`} />
-                                    {likesCount}
+                                <button onClick={toggleDislike} className='flex items-center'>
+                                    <FontAwesomeIcon icon={faThumbsDown} className={`mr-2 transition duration-200 ${isDisliked ? 'hover:text-hoverRed text-lightRed' : 'hover:text-hoverTextGrey text-textGrey'}`} />
+                                    {dislikesCount}
                                 </button>
                             </div>
                             {comment.replies !== null ?
