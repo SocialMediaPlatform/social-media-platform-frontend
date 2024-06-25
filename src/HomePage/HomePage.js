7e85c85faa62d91bf6807c6bf1a265172e88f1e5
mrozek <01178582@pw.edu.pl>
2024-06-25 22:37:07 +0200

    REFACTOR: Removed unecessary commented section

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -5,57 +5,6 @@ import MainFeed from './components/MainFeed';
 import Conversations from './components/Conversations';
 
 const HomePage = () => {
-    const { userToken } = useContext(AuthContext);
-    
-    //I need that code cause I decided to implement GET requests in MainFeed.js and ChatSidebar files,
-    //Mainfeed.js is ready but I still need to fetch followed users 
-
-    //useEffect(() => {
-    //    if (userToken) {
-    //        const fetchPosts = async () => {
-    //            try {
-    //                const response = await fetch('/api/posts', {
-    //                    method: 'GET',
-    //                    headers: {
-    //                        'Authorization': `Bearer ${userToken}`
-    //                    },
-    //                    credentials: 'include'
-    //                });
-    //                if (!response.ok) {
-    //                    throw new Error(`${response.status}`);
-    //                }
-    //                const result = await response.json();
-    //                setPosts(result);
-    //            } catch (error) {
-    //                console.error('Error fetching posts:', error);
-    //            }
-    //        };
-    //
-    //        const fetchFollowedUsers = async() => {
-    //            try {
-    //                const response = await fetch('/api/followed', {
-    //                    method: 'GET',
-    //                    headers: {
-    //                        'Authorization': `Bearer ${userToken}`
-    //                    },
-    //                    credentials: 'include'
-    //                });
-    //                if (!response.ok) {
-    //                    throw new Error(`${response.status}`);
-    //                }
-    //                console.log(response)
-    //                const result = await response.json();
-    //                setFollowedUsers(result);
-    //            } catch (error) {
-    //                console.error('Error fetching followed users:', error);
-    //            }
-    //        };
-    //
-    //        fetchPosts();
-    //        fetchFollowedUsers();
-    //    }
-    //}, [userToken]);
-
     return (
         <div className='flex flex-col h-screen overflow-hidden'>
             <Header />
