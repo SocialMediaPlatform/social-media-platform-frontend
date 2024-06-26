0b8441c561e6af8c025c8f264f3e7e50ec695673
Władysław Młynik <01168900@pw.edu.pl>
2024-06-26 08:48:50 +0200

    FIX: Fixed logout

--- src/AuthContext.js
+++ src/AuthContext.js
@@ -102,15 +102,7 @@ export const AuthProvider = ({ children }) => {
 
     const logout = async () => {
         try {
-        //    const response = await fetch('/api/auth/logout', {
-        //        method: 'POST',
-        //        credentials: 'include'
-        //});
-        //
-        //if (!response.ok) {
-        //    throw new Error('Logout failed');
-        //}
-
+            localStorage.clear();
             setUserToken(null);
             setUserId(null);
         } catch (error) {
